2801d36498779275a6aee42a45a1e5390b6470f1
safvankvr12 <102141286+safvankvr12@users.noreply.github.com>
2022-06-08 11:21:12 +0530

    Add files via upload
    breezed

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+
+$('.slider1').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows:false,
+    dots:true
+  });
+          
+  $('.slider2').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 3,
+    arrows:true,
+    dots:false,
+    
+  });
